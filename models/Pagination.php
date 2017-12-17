edc4e039e423521aec7b4e4fb19624c50e2faec9
kavera <augustin.kavera@outlook.fr>
2017-12-17 09:59:02 +0100

    Class de pagination -> Teste de pagination Pagination.php?page=

---
+++ models/Pagination.php
@@ -0,0 +1,50 @@
+<?php
+namespace models;
+
+class Pagination
+{
+    var $data;
+
+    function paginate($values, $perPage) {
+        $totalValues = count($values);
+
+        if (isset($_GET['page'])) {
+            $currentPage = $_GET['page'];
+        } else {
+            $currentPage = 1;
+        }
+
+        $counts = ceil($totalValues / $perPage);
+
+        $param1 = ($currentPage - 1) * $perPage;
+        $this->data = array_slice($values, $param1, $perPage);
+
+        for ($x=1; $x<= $counts; $x++) {
+            $numbers[] = $x;
+        }
+
+        return $numbers;
+
+    }
+
+
+    function fetchResult()
+    {
+        $resultsValues = $this->data;
+        return $resultsValues;
+    }
+
+}
+
+
+$page = new Pagination();
+$data = array("hey","Salut","Test de pagination");
+$numbers = $page->paginate($data,1);
+
+$result = $page->fetchResult();
+
+
+foreach ($result as $res) {
+    echo '<div>'.$res.'</div>';
+}
+
